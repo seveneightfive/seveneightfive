b90a879171d0a179c1551b2f7abfac397945ef20
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2026-03-14 18:14:23 -0500

    Update Stripe API version in stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,4 @@
 import Stripe from 'stripe'
-import { ApiVersion } from 'stripe/types/apiVersion'
 
 if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('Missing STRIPE_SECRET_KEY env var')
@@ -10,7 +9,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
  * Use STRIPE_SECRET_KEY (no NEXT_PUBLIC_ prefix).
  */
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: ApiVersion,
+  apiVersion: '2026-02-25.clover' as const,
   typescript: true,
 })
 
